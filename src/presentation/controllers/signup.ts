f81f2780c58294b63f382f58eda5da45a8f19f18
Nuno Miguel Mvemba <nunosirijc23@gmail.com>
2023-04-19 18:59:46 +0100

    feat: ensure SignUpcontroller returns 400 if an invalid email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,17 @@
 import { type HttpResponse, type HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, createdRequest } from '../helpers/http-helper'
 import { type Controller } from '../protocols/controller'
+import { type EmailValidator } from '../protocols/email-validator'
+import { InvalidParamError } from '../errors/invalid-param-error'
 
 export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
@@ -12,9 +20,11 @@ export class SignUpController implements Controller {
       }
     }
 
-    return {
-      statusCode: 400,
-      body: {}
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'))
     }
+
+    return createdRequest('Account Created Successfully')
   }
 }
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -4,3 +4,11 @@ export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
 })
+
+export const createdRequest = (message: string): HttpResponse => ({
+  statusCode: 201,
+  body: {
+    success: true,
+    message
+  }
+})
